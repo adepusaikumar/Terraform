de4f2548dbedd078f1f2db48e0529c03c4ec3cb6
saikumar001 <adepusaikumar0076@gmail.com>
2026-02-22 22:20:53 +0530

    Add comments to clarify dependencies in ec2.tf and r53.tf

--- count/r53.tf
+++ count/r53.tf
@@ -1,8 +1,8 @@
 resource "aws_route53_record" "www" {
   count = 10
   zone_id = var.zone_id
-  name    = "${var.instances[count.index]}.${var.domain_name}"
+  name    = "${var.instances[count.index]}.${var.domain_name}" 
   type    = "A"
   ttl     = 1
-  records = [aws_instance.example[count.index].private_ip]
+  records = [aws_instance.example[count.index].private_ip] # It means it depends on creation of ec2-intstances for private IP
 }

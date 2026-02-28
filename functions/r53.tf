d4fd4076faa2ecf7441984098c21b07b1ab624d2
saikumar001 <adepusaikumar0076@gmail.com>
2026-02-28 23:38:49 +0530

    fc

--- functions/r53.tf
+++ functions/r53.tf
@@ -1,10 +1,10 @@
 resource "aws_route53_record" "www" {
-  count = length(var.instances)
+  for_each=aws_instance.example
   zone_id = var.zone_id
   name    = "${var.instances[count.index]}.${var.domain_name}" 
   type    = "A"
   ttl     = 1
-  records = [aws_instance.example[count.index].private_ip] # It means it depends on creation of ec2-intstances for private IP
+  records = [each.private_ip] # It means it depends on creation of ec2-intstances for private IP
 }
 
 resource "aws_route53_record" "frontend"{

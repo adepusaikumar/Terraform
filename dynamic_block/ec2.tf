1bb5a75eb156868fc90aaa773ec0004b2466e99e
saikumar001 <adepusaikumar0076@gmail.com>
2026-02-28 21:26:16 +0530

    dynamic

---
+++ dynamic_block/ec2.tf
@@ -0,0 +1,38 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = "terraform"
+    Project = "roboshop"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow-all-terraform" # this is for AWS account
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  dynamic "ingress_block"{
+    for_each = var.ingress_rules
+    content{
+      from_port = ingress_block.value.from_port
+      to_port = ingress_block.value.to_port
+      description= ingress_block.key.description
+      cidr_blocks=ingress_block.value.cidr_blocks
+      ipv6_cidr_blocks=ingress_block.value.ipv6_cidr_blocks
+    } 
+  }
+
+  tags = {
+    Name = "allow-all-terraform"
+  }
+}
---
+++ dynamic_block/variables.tf
@@ -0,0 +1,50 @@
+# Map
+/* variable "instances" {
+    type = map
+    default = {
+        mongodb = "t3.micro"
+        mysql = "t3.small"
+        user = "t3.micro"
+        shipping = "t3.small"
+        frontend = "t3.micro"
+    }
+} */
+
+# This should be converted into set
+variable "instances" {
+    type = list
+    default = ["mongodb", "redis", "frontend"]
+}
+
+variable "zone_id" {
+  type = string
+  default = "Z101804623RU7HOQEG0G3"
+}
+
+variable "domain_name" {
+    default = "rajudevops.online"
+}
+
+variable "ingress_rules"{
+    type=list
+
+    default[
+    {
+        description = "Allow SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    {
+        description = "Allow HTTP"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    
+    ]
+}

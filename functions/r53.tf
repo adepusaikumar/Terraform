3f12356c2ce08a34263352d23aae60b6ec30143f
saikumar001 <adepusaikumar0076@gmail.com>
2026-02-28 23:44:29 +0530

    fc

--- functions/r53.tf
+++ functions/r53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "www" {
   for_each=aws_instance.example
   zone_id = var.zone_id
-  name    = "${var.instances[count.index]}.${var.domain_name}" 
+  name    = "${var.instances[each.key]}.${var.domain_name}" 
   type    = "A"
   ttl     = 1
   records = [each.value.private_ip] # It means it depends on creation of ec2-intstances for private IP

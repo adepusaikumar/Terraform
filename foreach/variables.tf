92fd586cb22fd6f8a611b5b44f7c358b238579bd
saikumar001 <adepusaikumar0076@gmail.com>
2026-02-28 21:01:29 +0530

    for each toset

--- foreach/variables.tf
+++ foreach/variables.tf
@@ -1,5 +1,5 @@
 # Map
-variable "instances" {
+/* variable "instances" {
     type = map
     default = {
         mongodb = "t3.micro"
@@ -8,13 +8,13 @@ variable "instances" {
         shipping = "t3.small"
         frontend = "t3.micro"
     }
-}
+} */
 
 # This should be converted into set
-/* variable "instances" {
+variable "instances" {
     type = list
     default = ["mongodb", "redis"]
-} */
+}
 
 variable "zone_id" {
   type = string

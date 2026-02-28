498f3448c3ed6b3c3bb1253bb9c9518ecf1b031d
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-01 00:14:01 +0530

    fc

--- functions/ec2.tf
+++ functions/ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "example" {
   for_each = toset(var.instances)
   # for_each = var.instances
   ami           = "ami-0220d79f3f480ecf5"
-  instance_type = each.value
+  instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = {

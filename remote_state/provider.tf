d5f9e7f56f9a1c6319ff65d8f6b30fe55488637d
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-01 20:49:23 +0530

    remote-state

--- remote_state/provider.tf
+++ remote_state/provider.tf
@@ -10,12 +10,12 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+}
 
-  backend "s3" {
+backend "s3" {
     bucket         = "remote-state-daws88s-sai"
     key            = "remote-state-demo-ec2.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile = true # Use S3 native locking (Terraform 1.10+)
   }
-}

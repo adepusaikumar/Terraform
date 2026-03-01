3a451490b053b338fd29bc8de52e909b1e807f9d
saikumar001 <adepusaikumar0076@gmail.com>
2026-03-01 20:27:03 +0530

    locals

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -3,8 +3,8 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-  tags = {
     count = length(local.instance_tags)
+  tags = {
     Name = "terraform"
     Project = "roboshop"
     Additional_tags = count.index
